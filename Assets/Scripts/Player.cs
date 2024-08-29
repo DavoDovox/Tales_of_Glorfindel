9b435894c60c6aa27c4d48f8f3ae9f5ff5b1a33d
PinguinoJhonny <a.bernacchi06@gmail.com>
2024-08-29 18:14:44 +0200

    Aggiunte animazioni personaggio, sprint, collisioni con albero bello

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
     
     public Enums enums;
+    public string PG;
     byte[] stats = {0, 0, 0, 0, 0};
     public float health;
     public Rigidbody2D rb;
     Vector2 velocity;
-    public const float speedMultiplier = 4.0f;
-    
+    public float speedMultiplier = 10.0f;
+    private Animator animator;
+    private string a_name;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        PG = "W_";
         health = 100;
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (PG == "W_")
+                PG = "T_";
+            else
+                PG = "W_";
+        }
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            speedMultiplier = 15.0f;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+                speedMultiplier = 10.0f;
+        if (velocity.x == 0 && velocity.y == 0){
+            if (a_name == PG+"walking_N")
+                Animation_update(PG + "idle_N");
+            else if (a_name == PG + "walking_S")
+                Animation_update(PG + "idle_S");
+            else if (a_name == PG + "walking_E")
+                Animation_update(PG + "idle_E");
+            else if (a_name == PG + "walking_W")
+                Animation_update(PG + "idle_W");
+        }
+        else{
+            if (velocity.x > 0)
+                Animation_update(PG + "walking_E");
+            else if (velocity.x < 0)
+                Animation_update(PG + "walking_W");
+            else if (velocity.y > 0)
+                Animation_update(PG + "walking_N");
+            else if (velocity.y < 0)
+                Animation_update(PG + "walking_S");
+        }
+    }
+
+    void Animation_update(string name)
+    {
+        if (name != a_name){
+            a_name = name;
+            animator.Play(a_name);
+        }
     }
 
     void FixedUpdate(){
         rb.MovePosition(rb.position + velocity * speedMultiplier * Time.fixedDeltaTime);
     }
+
+    
 }
